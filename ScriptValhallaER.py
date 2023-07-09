ae98cff21a01f36283b467777c6f7bd5e2ee5e39
Andres Cordeiro <56237736+cordeiroandres@users.noreply.github.com>
2023-07-09 16:35:16 +0200

    Update ScriptValhallaER.py

--- ScriptValhallaER.py
+++ ScriptValhallaER.py
@@ -64,12 +64,8 @@ if __name__ == '__main__':
         
         cb.srtm_assign(df_list)
         
-        df = df_list.to_numpy()
-        print('df=',df.head(3))
-        for row in df:   
-            c+=1
-            if c==4:
-                break
+        df = df_list.to_numpy()        
+        for row in df:                       
             next_p = row        
             if first_iteration:
                 uid = row[1]            
